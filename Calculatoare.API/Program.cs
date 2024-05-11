b5a8486d6b32eb544c14864fedc5f11a865abba7
omar <creepshaorma@gmail.com>
2024-05-11 17:00:33 +0300

    added database project

--- Calculatoare.API/Program.cs
+++ Calculatoare.API/Program.cs
@@ -1,9 +1,20 @@
+using Calculatoare.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+#region Swagger
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
+
+#region Database
+builder.Services.AddDbContext<CalculatoareContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+#endregion
 
 var app = builder.Build();
 
